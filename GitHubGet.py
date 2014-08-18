1a623f2d62cd469dfd67fec181cf13588b9a0d98
cclauss <cclauss@bluewin.ch>
2014-08-18 23:44:19 +0200

    Minor cleanup

--- GitHubGet.py
+++ GitHubGet.py
@@ -5,53 +5,38 @@
 # 
 ##   javascript:(function()%7Bif(document.location.href.indexOf('http')===0)document.location.href='pythonista://GitHubGet?action=run&argv='+document.location.href;%7D)();
 
-
-import urllib,zipfile,sys, clipboard, functools, re, os, tempfile
+import clipboard, functools, os, re, sys, tempfile, urllib, zipfile
 
 def extract_git_id(git):
-    print git
-    m = re.match((r'^http(s?)://([\w-]*\.)?github\.com/(?P<user>[\w-]+)/(?P<repo>[\w-]*)'
+    print('extract_git_id({})'.format(git)),
+    return re.match((r'^http(s?)://([\w-]*\.)?github\.com/(?P<user>[\w-]+)/(?P<repo>[\w-]*)'
                  '((/tree|/blob)/(?P<branch>[\w-]*))?'), git)
-#    print m.groupdict()
-    return m
     
 def git_download_from_args(args):
-    if len(args) == 2:
-        url = args[1]
-    else:
-        url = clipboard.get()
+    url = args[0] if args else clipboard.get()
     git_download(url)
 
-
 def dlProgress(filename, count, blockSize, totalSize):
-    if count*blockSize > totalSize:
-        percent=100
-    else:
-        percent = max(min(int(count*blockSize*100/totalSize),100),0)
+    percent = 100 if count*blockSize > totalSize else max(min(int(count*blockSize*100/totalSize),100),0)
     sys.stdout.write("\r" + filename + "...%d%%" % percent)
     sys.stdout.flush()
 
 def git_download(url):
-    base='https://codeload.github.com'
-    archive='zip'
     m=extract_git_id(url)
-    if m:
-        g=m.groupdict()
-        if not g['branch']:
-            g['branch']='master'
-
-        u=   '/'.join((base,g['user'],g['repo'],archive, g['branch']))
-        #print u
-        try:
-            with tempfile.NamedTemporaryFile(mode='w+b',suffix='.zip') as f:
-                urllib.urlretrieve(u,f.name,reporthook=functools.partial(dlProgress,u))
-                z=zipfile.ZipFile(f)
-                z.extractall()
-                print z.namelist()
-        except:
-            print('git url did not return zip file')
-    else:
-        print('could not determine repo url from clipboard or argv')
+    if not m:
+        print('could not determine repo url from ' + url)
+        return
+    g=m.groupdict()
+    g['branch'] = g['branch'] or 'master'
+    url = 'https://codeload.github.com/{user}/{repo}/zip/{branch}'.format(**g)
+    try:
+        with tempfile.NamedTemporaryFile(mode='w+b',suffix='.zip') as f:
+            urllib.urlretrieve(url,f.name,reporthook=functools.partial(dlProgress,url))
+            z=zipfile.ZipFile(f)
+            z.extractall()
+            print('\n{}\n'.format('\n'.join(z.namelist())))
+    except Exception as e:
+        print('git url did not return zip file\nException: ' + str(e))
         
 if __name__=='__main__':
-    git_download_from_args(sys.argv)
+    git_download_from_args(sys.argv[1:])
